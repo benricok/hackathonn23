9e59e60ea2df713fcf754956c8f8c93b56364486
Benrico Krog <benricok@krog.dev>
2023-10-07 07:36:54 +0200

    add rss pull

---
+++ testing/rss/pull-rss.py
@@ -0,0 +1,36 @@
+import feedparser
+import pandas as pd
+from datetime import datetime
+
+## Google News for rss
+
+# https://news.google.com/search?q=central+bank+US+EU+site%3Abloomberg.com+when%3A1d&hl=en-ZA&gl=ZA&ceid=ZA%3Aen
+#                                               ^
+#                                               Could possibly swop with SYMBOL tags 
+
+feed = feedparser.parse("https://news.google.com/rss/search?q=central+bank+US+EU+site%3Abloomberg.com+when%3A1d&hl=en-ZA&gl=ZA&ceid=ZA%3Aen")
+
+print('Number of RSS posts :', len(feed.entries))
+
+if len(feed.entries) == 0:
+    exit()
+
+titles = []
+dates = []
+
+for post in feed.entries:
+    title = post.title
+    date = post.published
+    titles.append(title.replace(' - Bloomberg', ''))
+    dates.append(datetime.strptime(date, '%a, %d %b %Y %X %Z').date())
+    #dates.append(datetime.strptime(date, '%c %Z').date())
+
+data = {'Title': titles,
+        'Date': dates}
+
+print(dates)
+
+df = pd.DataFrame(data)
+
+df.info()
+df.tail()
